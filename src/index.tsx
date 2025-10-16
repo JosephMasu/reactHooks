cf1fff82e2b7f8083989833f617a04d300d5264a
josephMasu <josephmuhindo089@gmail.com>
2025-10-17 00:11:01 +0300

    useImparativeHandle

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import CounterReducer from './hooks/useReducer'
+import ForwardRef  from './hooks/useImperativeHandle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CounterReducer />
+    <ForwardRef />
   </React.StrictMode>   
 );
 
